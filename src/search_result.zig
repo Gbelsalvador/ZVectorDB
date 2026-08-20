9ef0754a8fdfb19c683f69c6034c194e38acf1f1
gb el salvador <gbelsalvador6@gmail.com>
2026-08-20 07:10:56 +0100

    feat(serach_result) : ajout de recherche pour le topk qui sera utiliser pour le benchmark

--- src/search_result.zig
+++ src/search_result.zig
@@ -1,4 +1,48 @@
+const std = @import("std");
 pub const SearchResult = struct {
     document_id: usize,
     score: f64,
 };
+
+pub const SearchResult2 = struct { id: usize, score: f32 };
+
+pub fn topK(
+    allocator: std.mem.Allocator,
+    Candidates: []const SearchResult2,
+    k: usize,
+) ![]SearchResult2 {
+    var results = try allocator.dupe(
+        SearchResult2,
+        Candidates,
+    );
+
+    std.sort.pdq(
+        SearchResult2,
+        results,
+        {},
+        struct {
+            fn lessThan(
+                _: void,
+                a: SearchResult2,
+                b: SearchResult2,
+            ) bool {
+                return a.score > b.score;
+            }
+        }.lessThan,
+    );
+
+    const count = @min(k, results.len);
+    const output = try allocator.alloc(
+        SearchResult2,
+        count,
+    );
+
+    @memcpy(
+        output,
+        results[0..count],
+    );
+
+    allocator.free(results);
+
+    return output;
+}
